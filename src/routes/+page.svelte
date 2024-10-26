c642795000b1d4b4529d2e9b7de706f8f8355890
s1n7ax <srineshnisala@gmail.com>
2024-10-26 22:51:11 +0530

    chore: minor changes to base template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	interface Data {
 		visited: boolean;
 	}
 
 	const { data }: { data: Data } = $props();
+	let songs = $state();
+	let hasDownloadedSongs = $state(false);
+	onMount(() => {
+		const songsBlob = localStorage.getItem('songs');
+		hasDownloadedSongs = Boolean(songsBlob);
+
+		if (hasDownloadedSongs) {
+			songs = songsBlob;
+		}
+	});
 </script>
 
-<h1 class="text-9xl">{data.visited ? 'already a user' : 'first time'}</h1>
+<main class="grid place-content-center py-5">
+	<h1 class="text-1xl col-span-2 text-center">Open සිංහල Lyrics</h1>
+	{#if !hasDownloadedSongs}
+		<p>Looks like you have not downloaded any songs</p>
+	{/if}
+	<p></p>
+</main>
