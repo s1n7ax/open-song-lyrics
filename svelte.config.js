66116a3e8bd7da744ff2abe32f8cf91f1424600b
s1n7ax <srineshnisala@gmail.com>
2024-11-03 14:52:07 +0530

    fix(ci): invalid base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -18,7 +16,7 @@ const config = {
 			precompress: true
 		}),
 		paths: {
-			base: dev ? '' : 'https://s1n7ax.github.io/open-song-lyrics/'
+			base: '/open-song-lyrics'
 		}
 	}
 };
