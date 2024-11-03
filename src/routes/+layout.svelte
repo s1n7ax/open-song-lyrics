d2d04bf80ddb35a3c22e4fec2460bea33f303522
s1n7ax <srineshnisala@gmail.com>
2024-11-03 15:07:14 +0530

    fix: full width on mobile screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,9 +16,9 @@
 	});
 </script>
 
-<div class="grid h-full w-full grid-cols-1 grid-rows-1 justify-items-center p-3">
+<div class="grid h-full w-full grid-cols-1 grid-rows-1 justify-items-center p-0 sm:p-3">
 	<div
-		class="grid w-11/12 grid-rows-[auto_1fr] gap-3 overflow-hidden rounded-md bg-slate-100 p-3 md:w-8/12 lg:w-7/12"
+		class="grid w-full grid-rows-[auto_1fr] gap-3 overflow-hidden rounded-md bg-slate-100 p-3 sm:w-11/12 md:w-8/12 lg:w-7/12"
 	>
 		<h1 class="text-center text-3xl md:text-4xl lg:text-5xl">Open සිංහල Lyrics</h1>
 		{#if songState.isLoadingSongs}
